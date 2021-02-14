d4f76b8d229b79b522ebaa601cc4f381f5fe803f
Ahmed younes <ahyounes2015@gmail.com>
2021-02-14 03:43:58 +0000

    create home page

---
+++ mycoffee/screens/home.js
@@ -0,0 +1,47 @@
+//import { AsyncLocalStorage } from 'async_hooks';
+//import Sign_Up from './components/singup';
+import React, {Component} from 'react';
+import {Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+class HomeScreen extends Component {
+    componentDidMount() {
+        this.unsubscribe = this.props.navigation.addListener('focus', () => {
+            this.checkLoggedIn();
+
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    checkLoggedIn = async () => {
+
+        const value = await AsyncStorage.getItem('@session_token');
+        if (value == null) {
+            this.props.navigation.navigate('login');
+        }
+    };
+
+    render() {
+        return (
+            <View style= {{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+
+            }}>
+            <Text>Home</Text>
+
+            </View>
+        );
+    }
+
+
+}
+
+export default HomeScreen;
