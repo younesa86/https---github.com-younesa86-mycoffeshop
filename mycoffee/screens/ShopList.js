0c583b1e4c08a1895bdb37f9b30a9bc3c3e820e1
Ahmed younes <ahyounes2015@gmail.com>
2021-02-17 23:43:57 +0000

    edit get all coffee

--- mycoffee/screens/ShopList.js
+++ mycoffee/screens/ShopList.js
@@ -4,19 +4,19 @@ import {
   ScrollView,
   ToastAndroid,
   StyleSheet, Text,
-  TextInput,
+  Dimensions,
   TouchableOpacity, View
 } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
+const {width} = Dimensions.get('screen')
 class UpdateUser extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isloading: true,
+      
       listData: []
 
     }
@@ -49,7 +49,6 @@ class UpdateUser extends Component {
     })
     .then( (responseJson) => {
         this.setState({
-            isloading: false,
             listData: responseJson
         })
        
@@ -71,10 +70,20 @@ class UpdateUser extends Component {
      
     
     return (
+      <View style= {{ flex: 1, backgroundColor: 'black', alignItems: 'center' }} >
+
+      <View style= {{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text style= {{fontSize: 20, color: 'white', marginLeft: 10, padding: 20}} > Coffee Location</Text>
+      </View>
+
+      <View style= {{width: width- 40, marginBottom: 30, backgroundColor: 'white', padding: 30, borderRadius: 10}}>
+
       
-      <View>
+      
        <FlatList
-                data={this.state.listData}
+                nestedScrollEnabled
+                style ={{maxHeight: 75*5}}
+                data={this.state.listData || []}
                 renderItem={({item}) => (
                     <View>
                       <Text>{item.location_name}</Text>
@@ -83,6 +92,8 @@ class UpdateUser extends Component {
                 )}
                 keyExtractor={(item,index) => item.location_id.toString()}
               />
+
+       </View>
 
       </View>
     )}
---
+++ mycoffee/screens/UserInfo.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  ToastAndroid,
+  StyleSheet, Text,
+  TextInput,
+  TouchableOpacity, View
+} from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+class UpdateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      user_id: "",
+      favourite_locations: [],
+       
+
+
+    }
+  }
+
+  componentDidMount() {
+      this.getinfo()
+
+  }
+
+  //get user infoo
+  getinfo = async () => {
+      
+    let user_Id= await AsyncStorage.getItem('@user_id');
+    let token = await AsyncStorage.getItem('@session_token');
+    return fetch("http://10.0.2.2:3333/api/1.0.0/user/" + user_Id, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-authorization': token
+
+      }
+      
+    })
+    .then((response) => {
+        if(response.status === 200) {
+            return response.json()
+        }else {
+            throw 'Something went wrong';
+        }
+    })
+    .then(async (responseJson) => {
+        this.setState({"first_name": responseJson.first_name})
+        this.setState({"last_name": responseJson.last_name})
+        this.setState({"email": responseJson.email})
+      })
+      .catch((error) => {
+        console.log(error);
+        ToastAndroid.show(error, ToastAndroid.SHORT);
+      })
+
+    
+  }
+
+
+
+
+  
+  
+
+  render() {
+    
+    return (
+      <View>
+        <ScrollView>
+          <Text style={styles.title}>User Information</Text>
+
+          <View style={styles.formItem}>
+            <Text style={styles.formLabel}>First Name:</Text>
+            <TextInput
+             
+              style={styles.formInput}
+              onChangeText={(first_name) => this.setState({first_name})}
+              value={this.state.first_name}
+            />
+          </View>
+
+          <View style={styles.formItem}>
+            <Text style={styles.formLabel}>Last Name:</Text>
+            <TextInput
+             
+              style={styles.formInput}
+              onChangeText={(last_name) => this.setState({last_name})}
+              value={this.state.last_name}
+            />
+          </View>
+
+          <View style={styles.formItem}>
+            <Text style={styles.formLabel}>Email:</Text>
+            <TextInput
+             
+              style={styles.formInput}
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email}
+            />
+          </View>
+
+          <View style={styles.formItem}>
+            <Text style={styles.formLabel}>Password:</Text>
+            <TextInput
+             
+              style={styles.formInput}
+              secureTextEntry
+              onChangeText={(password) => this.setState({password})}
+              value={this.state.password}
+            />
+          </View>
+
+          <View style={styles.formItem}>
+            <TouchableOpacity
+              style={styles.formTouch}
+              onPress={ this.editInfo}
+              >
+              <Text style={styles.formTouchText}>save</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'steelblue',
+    backgroundColor: 'lightblue',
+    padding: 10,
+    fontSize: 25,
+  },
+  formItem: {
+    padding: 20,
+  },
+  formLabel: {
+    fontSize: 15,
+    color: 'steelblue',
+  },
+  formInput: {
+    borderWidth: 1,
+    borderColor: 'lightblue',
+    borderRadius: 5,
+  },
+  formTouch: {
+    backgroundColor: 'lightblue',
+    padding: 10,
+    alignItems: 'center',
+  },
+  formTouchText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'steelblue',
+  },
+});
+
+export default UpdateUser;
