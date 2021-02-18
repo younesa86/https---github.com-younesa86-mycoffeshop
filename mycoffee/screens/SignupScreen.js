2fb7a276cd89a945d753a5ca39bb728fef05fd88
Ahmed younes <ahyounes2015@gmail.com>
2021-02-18 19:02:48 +0000

    update

--- mycoffee/screens/SignupScreen.js
+++ mycoffee/screens/SignupScreen.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import {
   ScrollView,
-  Button,
   ToastAndroid,
-  StyleSheet, Text,
+   Text,
   TextInput,
   TouchableOpacity, View
 } from 'react-native';
@@ -54,54 +53,73 @@ class SignupScreen extends Component {
     return (
       <View>
         <ScrollView>
-          <Text style={styles.title}>Create an account</Text>
+          <Text style={{color: 'steelblue',
+                        backgroundColor: 'lightblue',
+                        padding: 10,
+                        fontSize: 25}}>Create an account</Text>
 
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>First Name:</Text>
+          <View style={{padding: 20}}>
+            <Text style={{fontSize: 15,
+                          color: 'steelblue'}}>First Name:</Text>
             <TextInput
               placeholder="Enter first name..."
-              style={styles.formInput}
+              style={{ borderWidth: 1,
+                        borderColor: 'lightblue',
+                        borderRadius: 5}}
               onChangeText={(first_name) => this.setState({first_name})}
               value={this.state.first_name}
             />
           </View>
 
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Last Name:</Text>
+          <View style={{padding: 20}}>
+            <Text style={{fontSize: 15,
+                          color: 'steelblue'}}>Last Name:</Text>
             <TextInput
               placeholder="Enter last name..."
-              style={styles.formInput}
+              style={{ borderWidth: 1,
+                        borderColor: 'lightblue',
+                        borderRadius: 5}}
               onChangeText={(last_name) => this.setState({last_name})}
               value={this.state.last_name}
             />
           </View>
 
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Email:</Text>
+          <View style={{padding: 20}}>
+            <Text style={{fontSize: 15,
+                          color: 'steelblue'}}>Email:</Text>
             <TextInput
               placeholder="Enter an email..."
-              style={styles.formInput}
+              style={{ borderWidth: 1,
+                        borderColor: 'lightblue',
+                        borderRadius: 5}}
               onChangeText={(email) => this.setState({email})}
               value={this.state.email}
             />
           </View>
 
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Password:</Text>
+          <View style={{padding: 20}}>
+            <Text style={{fontSize: 15,
+                          color: 'steelblue'}}>Password:</Text>
             <TextInput
               placeholder="Enter password..."
-              style={styles.formInput}
+              style={{ borderWidth: 1,
+                        borderColor: 'lightblue',
+                        borderRadius: 5}}
               secureTextEntry
               onChangeText={(password) => this.setState({password})}
               value={this.state.password}
             />
           </View>
 
-          <View style={styles.formItem}>
+          <View style={{padding: 20}}>
             <TouchableOpacity
-              style={styles.formTouch}
+              style={{ backgroundColor: 'lightblue',
+                        padding: 10,
+                        alignItems: 'center'}}
               onPress={() => this.SignUp()}>
-              <Text style={styles.formTouchText}>Sign Up</Text>
+              <Text style={{fontSize: 20,
+                            fontWeight: 'bold',
+                            color: 'steelblue'}}>Sign Up</Text>
             </TouchableOpacity>
           </View>
         </ScrollView>
@@ -110,35 +128,6 @@ class SignupScreen extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  title: {
-    color: 'steelblue',
-    backgroundColor: 'lightblue',
-    padding: 10,
-    fontSize: 25,
-  },
-  formItem: {
-    padding: 20,
-  },
-  formLabel: {
-    fontSize: 15,
-    color: 'steelblue',
-  },
-  formInput: {
-    borderWidth: 1,
-    borderColor: 'lightblue',
-    borderRadius: 5,
-  },
-  formTouch: {
-    backgroundColor: 'lightblue',
-    padding: 10,
-    alignItems: 'center',
-  },
-  formTouchText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'steelblue',
-  },
-});
+
 
 export default SignupScreen;
