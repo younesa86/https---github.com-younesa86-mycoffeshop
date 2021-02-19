73276062e60d42d14172fb43d101f14811e0499f
Ahmed younes <ahyounes2015@gmail.com>
2021-02-19 21:04:32 +0000

    update screen

--- mycoffee/App.js
+++ mycoffee/App.js
@@ -8,10 +8,12 @@ import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import UserScreen from './screens/UserScreen';
 import ShopList from './screens/ShopList';
+import UserFavourite from './screens/UserFavourite';
+import ShopDetailes from './screens/ShopDetailes';
 
 
 
-const appstart = createStackNavigator();
+const stack = createStackNavigator();
 
 class App extends React.Component{
   constructor(props){
@@ -28,15 +30,17 @@ class App extends React.Component{
 
       <NavigationContainer>
 
-        <appstart.Navigator initialRouteName="Home">
-          <appstart.Screen name={'LoginScreen'} component={LoginScreen} />
-          <appstart.Screen name={'SignupScreen'} component={SignupScreen} />
-          <appstart.Screen name={'HomeScreen'} component={HomeScreen} />
-          <appstart.Screen name={'UserScreen'} component={UserScreen} />
-          <appstart.Screen name={'ShopList'} component={ShopList} />
+        <stack.Navigator >
+          <stack.Screen name="LoginScreen" component={LoginScreen} options={{title:"Login"}}/>
+          <stack.Screen name="SignupScreen" component={SignupScreen} options={{title:"Login"}} />
+          <stack.Screen name="HomeScreen" component={HomeScreen} options={{title:"Home"}}/>
+          <stack.Screen name="UserScreen" component={UserScreen} options={{title:"Home"}}/>
+          <stack.Screen name="ShopList" component={ShopList} options={{title:"Home"}}/>
+          <stack.Screen name="UserFavourite" component={UserFavourite} options={{title:"SignUp"}}/>
+          <stack.Screen name="ShopDetailes" component={ShopDetailes} />
 
 
-        </appstart.Navigator>
+        </stack.Navigator>
            
 
 
