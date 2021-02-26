ed8cdb19e3613a951fa45012cd58e1119c92819f
Ahmed younes <ahyounes2015@gmail.com>
2021-02-26 19:37:17 +0000

    camera build

--- mycoffee/screens/Camera.js
+++ mycoffee/screens/Camera.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
-import {RNCamera} from 'react-native-camera'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, {Component} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Camera extends Component {
   constructor (props) {
@@ -12,9 +12,9 @@ class Camera extends Component {
       review_id: this.props.route.params.review_id,
     }
   }
-  componentDidMount () {
-    this.takereviewphoto()
-  }
+  // componentDidMount () {
+  //   this.takereviewphoto()
+  // }
 
   render () {
     return (
@@ -29,7 +29,7 @@ class Camera extends Component {
         <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
           <TouchableOpacity
             onPress={() => {
-              this.takereviewphoto()
+              this.takephoto()
             }}
             style={styles.capture}>
             <Text style={{fontSize: 16}}>CAPTURE</Text>
@@ -40,16 +40,17 @@ class Camera extends Component {
   }
 
   takereviewphoto = async () => {
-    let token = await AsyncStorage.getItem('@session_token')
 
     let loc_id = this.state.location_id
     let rev_id = this.state.review_id
-    const options = {quality: 0.5, base64: true}
+    const options = {quality:0.5 , base64: true}
     const data = await this.camera.takePictureAsync(options)
 
     console.log(data.uri)
-
     if (this.camera) {
+
+    let token = await AsyncStorage.getItem('@session_token')
+
       return fetch(
         'http://10.0.2.2:3333/api/1.0.0/location/' +
           loc_id +
@@ -64,10 +65,9 @@ class Camera extends Component {
           body: data,
         },
       )
-        .then(response => {
+        .then(response =>   {
           if (response.status === 200) {
             console.log('photo added')
-            ToastAndroid.show('unlike', ToastAndroid.show)
           } else {
             throw 'Something went wrong'
           }
@@ -79,15 +79,15 @@ class Camera extends Component {
     }
   }
 
-  //   takephoto = async() => {
-  //     if(this.camera) {
-  //       const options = { quality: 0.5, base64: true };
-  //       const data = await this.camera.takePictureAsync(options);
-  //       this.takereviewphoto(data)
+    takephoto = async() => {
+      if(this.camera) {
+        const options = { quality: 0.5, base64: true };
+        const data = await this.camera.takePictureAsync(options);
+        this.takereviewphoto(data)
 
-  //     }
+      }
 
-  //   }
+    }
 }
 
 const styles = StyleSheet.create({
@@ -103,4 +103,4 @@ const styles = StyleSheet.create({
   },
 })
 
-export default Camera
+export default Camera;
